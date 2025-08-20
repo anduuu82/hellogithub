0e3ff675616173ef0528c4278c2f29f970afdf2a
安東壱成 <isseiando@andouisseinoMacBook-Air.local>
2025-08-20 15:24:17 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('Progress bar')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!'
+
+
+left_column, right_column =st.columns(2)
+button = left_column.button('右からむに文字を表示')
+if button:
+    right_column.write('ここは右からむ')
+
+expander1 = st.expander('問い合わせ１')
+expander1.write('問い合わせ１の回答')
+expander2 = st.expander('問い合わせ２')
+expander2.write('問い合わせ２の回答')
